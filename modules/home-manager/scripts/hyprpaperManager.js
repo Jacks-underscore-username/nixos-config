56aab19fa2103c359ba3a2afdb9c95e7bf2e4bdf
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-11 00:00:35 -0500

    Generation #86

--- modules/home-manager/scripts/hyprpaperManager.js
+++ modules/home-manager/scripts/hyprpaperManager.js
@@ -4,7 +4,7 @@ const fs = require("node:fs");
 
 const assetsPath = path.resolve(process.argv[1], "../../../../assets");
 const papersPath = path.resolve(assetsPath, "wallpapers");
-const blankPath = path.resolve(assetsPath, "1x1_#000000FF.png");
+const blankPath = path.resolve(assetsPath, "black_pixel.png");
 fs.writeFileSync(
 	"/home/jackc/.config/hypr/hyprpaper.conf",
 	`preload = ${blankPath}\nwallpaper = ,${blankPath}`,
@@ -13,31 +13,36 @@ fs.writeFileSync(
 const allPapers = new Set(fs.readdirSync(papersPath));
 const lastPapers = new Set();
 
-while (true) {
-	try {
-		if (execSync("hyprctl hyprpaper").toString() === "invalid command\n") break;
-	} catch (e) {}
-	execSync("hyprctl dispatch exec hyprpaper");
-	await new Promise((r) => setInterval(r, 100));
-}
-
-const randomizePapers = () => {
-	const monitors = JSON.parse(execSync("hyprctl monitors -j").toString()).map(
-		(e) => e.name,
-	);
-	const currentPapers = new Set();
-	for (const name of monitors) {
-		const validPapers = allPapers
-			.difference(lastPapers)
-			.difference(currentPapers);
-		const paper =
-			Array.from(validPapers)[Math.floor(Math.random() * validPapers.size)];
-		currentPapers.add(paper);
-		const paperPath = path.resolve(papersPath, paper);
-		execSync(`hyprctl hyprpaper reload "${name},${paperPath}"`);
-	}
-	lastPapers.clear();
-	for (const paper of currentPapers) lastPapers.add(paper);
+const randomizePapers = async () => {
+	while (true)
+		try {
+			const monitors = JSON.parse(
+				execSync("hyprctl monitors -j").toString(),
+			).map((e) => e.name);
+			const currentPapers = new Set();
+			for (const name of monitors) {
+				const validPapers = allPapers
+					.difference(lastPapers)
+					.difference(currentPapers);
+				const paper =
+					Array.from(validPapers)[Math.floor(Math.random() * validPapers.size)];
+				currentPapers.add(paper);
+				const paperPath = path.resolve(papersPath, paper);
+				execSync(`hyprctl hyprpaper reload "${name},${paperPath}"`);
+			}
+			lastPapers.clear();
+			for (const paper of currentPapers) lastPapers.add(paper);
+			break;
+		} catch (e) {
+			while (true) {
+				try {
+					if (execSync("hyprctl hyprpaper").toString() === "invalid command\n")
+						break;
+				} catch (e) {}
+				execSync("hyprctl dispatch exec hyprpaper");
+				await new Promise((r) => setInterval(r, 100));
+			}
+		}
 };
 
 randomizePapers();
