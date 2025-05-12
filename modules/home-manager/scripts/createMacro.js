3277749ba84d84d2ad2ecbafc446b99d91c6f227
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-11 19:04:58 -0500

    Generation #94

--- modules/home-manager/scripts/createMacro.js
+++ modules/home-manager/scripts/createMacro.js
@@ -1,4 +1,4 @@
-const { exec } = require("node:child_process");
+const { spawn } = require("node:child_process");
 const path = require("node:path");
 const fs = require("node:fs");
 const readline = require("node:readline");
@@ -16,10 +16,17 @@ const askQuestion = (question) =>
 	new Promise((resolve) => rl.question(question, (answer) => resolve(answer)));
 
 const name = await askQuestion("What should the name of the macro be?: ");
-const mods = await askQuestion("What mod keys?: ");
-const key = await askQuestion("What key?: ");
+const hasTrigger =
+	(
+		await askQuestion("Does this macro need a key combo? (Y/n): ")
+	).toLowerCase() !== "n";
+const mods = hasTrigger ? await askQuestion("What mod keys?: ") : "";
+const key = hasTrigger ? await askQuestion("What key?: ") : "";
 
-if (name.length === 0 || mods.length === 0 || key.length === 0)
+if (
+	name.length === 0 ||
+	(hasTrigger && (mods.length === 0 || key.length === 0))
+)
 	throw new Error("Invalid input");
 if (fs.readdirSync(scriptsPath).some((n) => n.split(".")[0] === name))
 	throw new Error("That name is already used");
@@ -28,7 +35,10 @@ fs.writeFileSync(
 	path.resolve(scriptsPath, `${name}.nix`),
 	`{pkgs}:\npkgs.writeShellScriptBin "${name}" ''\n  bun "/persist/nixos/modules/home-manager/scripts/${name}.js"\n''`,
 );
-fs.writeFileSync(path.resolve(scriptsPath, `${name}.js`), "");
+fs.writeFileSync(
+	path.resolve(scriptsPath, `${name}.js`),
+	'const { execSync, spawn } = require("node:child_process");const path = require("node:path");\nconst fs = require("node:fs");\n\nconst statePath = `${process.argv[1].slice(0, process.argv[1].length - 2)}state`;',
+);
 fs.writeFileSync(
 	homePath,
 	fs
@@ -41,18 +51,25 @@ fs.writeFileSync(
 		)
 		.join("\n"),
 );
-fs.writeFileSync(
-	hyprlandPath,
-	fs
-		.readFileSync(hyprlandPath, "utf8")
-		.split("\n")
-		.map((l) =>
-			/# <MACRO INSERT>/.test(l)
-				? `${l}\n          "${mods},${key},exec,${name}"`
-				: l,
-		)
-		.join("\n"),
-);
+if (hasTrigger)
+	fs.writeFileSync(
+		hyprlandPath,
+		fs
+			.readFileSync(hyprlandPath, "utf8")
+			.split("\n")
+			.map((l) =>
+				/# <MACRO INSERT>/.test(l)
+					? `${l}\n          "${mods},${key},exec,${name}"`
+					: l,
+			)
+			.join("\n"),
+	);
 
-exec(`hyprctl dispatch exec "code ${path.resolve(scriptsPath, `${name}.js`)}"`);
+spawn(
+	`hyprctl dispatch exec "code ${path.resolve(scriptsPath, `${name}.js`)}"`,
+	{ detached: true },
+);
 console.log("Macro created!");
+console.log(
+	`Run it with "bun /${path.resolve(scriptsPath, `${name}.js`)}" or just ${name} after a reload`,
+);
