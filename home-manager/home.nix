f6c953fec4070438160e3cb42194d82fc8721ad2
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-11 20:59:08 -0500

    Generation #96

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -135,6 +135,7 @@
     (import ../modules/home-manager/scripts/toggleLightMode.nix {inherit pkgs;})
 
     python314
+    fuse
 
     (import ../modules/home-manager/scripts/createMacro.nix {inherit pkgs;})
     # <MACRO INSERT>
