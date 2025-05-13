3ffa344ad8d5597f765d92aa498a7be9924fcd9a
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-13 16:06:21 -0500

    Generation #108

--- modules/home-manager/scripts/copyJsConfig.nix
+++ modules/home-manager/scripts/copyJsConfig.nix
@@ -1,4 +1,4 @@
 {pkgs}:
 pkgs.writeShellScriptBin "copyJsConfig" ''
-  bun "/persist/nixos/modules/home-manager/scripts/copyJsConfig.js" "$\{caller 0}"
+  bun "/persist/nixos/modules/home-manager/scripts/copyJsConfig.js" $\{caller 0}
 ''
