d8fc42d0a93d8f6abf750570cc06fd6e29802aba
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-16 02:58:12 -0500

    Generation #115

--- shells/pico.nix
+++ shells/pico.nix
@@ -1,4 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [python314 pico-sdk openocd cmake ninja libusb1];
+  buildInputs = with pkgs; [libusb1]; # Try libusb1
 }
