a2af62af5b880adc2f393f9099c25d343cfcc0a4
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-16 02:18:55 -0500

    Generation #112

---
+++ modules/home-manager/scripts/shell.js
@@ -0,0 +1,4 @@
+const { execSync } = require('node:child_process')
+const path = require('node:path')
+
+execSync(`nix-shell ${path.join('/persist/nixos/shells', process.argv[2])}`)
---
+++ modules/home-manager/scripts/shell.nix
@@ -0,0 +1,4 @@
+{pkgs}:
+pkgs.writeShellScriptBin "shell" ''
+  bun "/persist/nixos/modules/home-manager/scripts/shell.js" $0
+''
