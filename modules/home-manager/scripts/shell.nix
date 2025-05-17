3ca2c8f5a4e5db36312423b2ef6abef0faa1b2cd
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-17 15:36:10 -0500

    Generation #140

--- modules/home-manager/scripts/shell.nix
+++ modules/home-manager/scripts/shell.nix
@@ -1,4 +1,8 @@
 {pkgs}:
 pkgs.writeShellScriptBin "shell" ''
-  nix-shell "/persist/nixos/shells/$1.nix"
+  if [ $# -ge 2 && "$2" == "pure" ]; then
+    nix-shell "/persist/nixos/shells/$1.nix" --pure
+  else
+    nix-shell "/persist/nixos/shells/$1.nix"
+  fi
 ''
