1dc5fccda7716837735f1e6bebf32528c8590641
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-17 15:05:37 -0500

    Generation #123

--- shells/pico.nix
+++ shells/pico.nix
@@ -1,11 +1,18 @@
-{pkgs ? import <nixpkgs> {}}: let
-  # Find the libusb library path dynamically (more robust)
-  libusbLibPath = "${pkgs.libusb1}/lib"; # Or ${pkgs.libusb1}/lib if you switched
+{
+  # Import the stable channel
+  pkgs ? import <nixpkgs> {},
+  # Import the unstable channel
+  pkgs_unstable ? import <nixpkgs> {config = {allowUnfree = true;};}, # Allow unfree if needed for unstable
+}: let
+  libusbLibPath = "${pkgs_unstable.libusb1}/lib";
+  picoSdkPath = "${pkgs_unstable.pico-sdk}/lib/pico-sdk";
+  deps = with pkgs_unstable.buildPackages; [python314 pico-sdk openocd cmake ninja gdb libftdi1 vscode git];
 in
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [python314 pico-sdk openocd cmake ninja gdb libftdi1];
-    buildInputs = with pkgs; [libusb1];
+    nativeBuildInputs = deps;
+    buildInputs = deps;
     shellHook = ''
       export LD_LIBRARY_PATH="${libusbLibPath}:$LD_LIBRARY_PATH"
+      export PICO_SDK_PATH="${picoSdkPath}$PICO_SDK_PATH"
     '';
   }
