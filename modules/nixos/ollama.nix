7a6d464a6c5e7f38cae1568e91e5c1e412e2c551
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-05-18 23:27:31 -0500

    Generation #147

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -2,7 +2,7 @@
   services.ollama = {
     enable = true;
     package = pkgs.unstable.ollama;
-    loadModels = ["phi4-reasoning:14b"];
+    loadModels = ["phi4-mini-reasoning:3.8b"];
   };
   services.open-webui = {
     enable = true;
