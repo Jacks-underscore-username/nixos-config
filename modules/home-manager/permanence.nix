058da3b675ac0fc256e7a45eb8bb53919ebbfc4c
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-07-13 12:41:45 -0500

    Generation #194

--- modules/home-manager/permanence.nix
+++ modules/home-manager/permanence.nix
@@ -2,8 +2,8 @@
   home.persistence."/persist/home/jackc" = {
     allowOther = true;
     directories = [
-      # ".local/share/PrismLauncher"
-      # ".ssh"
+      ".local/share/PrismLauncher"
+      ".ssh"
       ".config/gh"
       ".vscode"
       ".cache/google-chrome"
@@ -12,13 +12,9 @@
       ".cache/spotify"
       ".config/spotify"
       ".config/legcord"
-      # "Steam"
-      # ".pki"
-      # ".local/share/Steam"
-      # ".steam"
-
-      # ".local"
-      # ".cache"
+      "Steam"
+      ".local/share/Steam"
+      ".steam"
     ];
     files = [
       # ".bash_history"
