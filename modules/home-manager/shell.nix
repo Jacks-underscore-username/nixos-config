403eff1d9164638a72bbb0bc7e896a7d8cef2888
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-07-14 11:47:46 -0500

    Generation #201

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -18,11 +18,11 @@
 
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "sudo" "docker" "kubectl"];
+      plugins = ["git" "thefuck"];
+      theme = "robbyrussell";
     };
   };
 
   home.sessionVariables = {
-    SHELL = "zsh";
   };
 }
