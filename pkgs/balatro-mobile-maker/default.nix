34be9bd67db06e3f9c47ac7941979a5d67b573f3
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-06 22:42:15 -0500

    Generation #246

--- pkgs/balatro-mobile-maker/default.nix
+++
@@ -1,6 +0,0 @@
-# default.nix
-let
-  pkgs = import <nixpkgs> {};
-in {
-  balatro-mobile-maker = pkgs.callPackage ./balatro-mobile-maker.nix {};
-}
---
+++ pkgs/balatro-mobile-maker/deps.nix
@@ -0,0 +1,27 @@
+{fetchNuGet}: [
+  (fetchNuGet {
+    pname = "Microsoft.AspNetCore.App.Ref";
+    version = "8.0.11";
+    hash = "sha256-dXJ1h1xyeI+lzdoNiYtmLBzkQnHKZcWSksjuo70yp5k=";
+  })
+  (fetchNuGet {
+    pname = "Microsoft.NET.ILLink.Tasks";
+    version = "8.0.11";
+    hash = "sha256-szAnroFmCOKpUsq8JuwZvFujB63Tw1gp1AjvKzhWa3A=";
+  })
+  (fetchNuGet {
+    pname = "Microsoft.NETCore.App.Host.linux-x64";
+    version = "8.0.11";
+    hash = "sha256-brt8CP11GH1hidrkYbAou8mMQ6kr2eStr/oqesK6AnY=";
+  })
+  (fetchNuGet {
+    pname = "Microsoft.NETCore.App.Ref";
+    version = "8.0.11";
+    hash = "sha256-lo6MAnvFQ1DBDh+9qdxzOJMgACsvFjj2e5bhreJ4v0I=";
+  })
+  (fetchNuGet {
+    pname = "SharpZipLib";
+    version = "1.4.2";
+    hash = "sha256-/giVqikworG2XKqfN9uLyjUSXr35zBuZ2FX2r8X/WUY=";
+  })
+]
