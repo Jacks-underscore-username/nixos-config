3b49440b1f4751c7336d2f930a7d01c5fb8e980d
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-07 17:32:58 -0500

    Generation #249

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,13 +1,18 @@
 {pkgs, ...}: {
-  programs.ssh.extraConfig = {
-    "relay" = {
-      hostname = "170.205.31.42";
-      user = "jack";
-      port = 43;
-    };
-    "server" = {
-      hostname = "";
-      user = "jackc";
-    };
-  };
+  programs.ssh.extraConfig = ''
+    Host relay
+      Hostname 170.205.31.42
+      User jack
+      Port 43
+  '';
+  #     "relay" = {
+  #       hostname = "170.205.31.42";
+  #       user = "jack";
+  #       port = 43;
+  #     };
+  #     "server" = {
+  #       hostname = "";
+  #       user = "jackc";
+  #     };
+  #   };
 }
