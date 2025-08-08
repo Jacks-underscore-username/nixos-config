9807c95e6851ef9e2edb38c095b584e250636ee5
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-08 08:56:02 -0500

    Generation #257

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -37,13 +37,13 @@
                     "$@" \
                     | awk " \
                         BEGIN { \
-                        FS=\"\\t\"; \
-                        printf \"\n\"; \
-                        last_commit=\"\"; \
+                        FS="\\t"; \
+                        printf "\n"; \
+                        last_commit=""; \
                         } \
                         /^.{40} - / { \
-                        if (last_commit != \"\") { \
-                            printf \"\n\"; \
+                        if (last_commit != "") { \
+                            printf "\n"; \
                         } \
                         print \$0; \
                         last_commit = \$1; \
@@ -54,25 +54,25 @@
                         dels=\$2; \
                         filepath=\$3; \
                         \
-                        color_code=\"\"; \
-                        diff_summary=\"\"; \
+                        color_code=""; \
+                        diff_summary=""; \
                         \
                         # Determine status based on additions/deletions (heuristic) \
-                        if (adds == \"-\" && dels != \"-\") { \
-                            color_code=\"\\033[0;31m\"; /* Red for Deleted */ \
-                            diff_summary=\"(Deleted)\"; \
-                        } else if (adds != \"-\" && dels == \"-\") { \
-                            color_code=\"\\033[0;32m\"; /* Green for Added */ \
-                            diff_summary=\"(Added)\"; \
-                        } else if (adds != \"-\" && dels != \"-\") { \
-                            color_code=\"\\033[0;33m\"; /* Yellow for Modified */ \
-                            diff_summary=\Sprintf(\"(+%s, -%s)\", adds, dels); \
+                        if (adds == "-" && dels != "-") { \
+                            color_code="\\033[0;31m"; /* Red for Deleted */ \
+                            diff_summary="(Deleted)"; \
+                        } else if (adds != "-" && dels == "-") { \
+                            color_code="\\033[0;32m"; /* Green for Added */ \
+                            diff_summary="(Added)"; \
+                        } else if (adds != "-" && dels != "-") { \
+                            color_code="\\033[0;33m"; /* Yellow for Modified */ \
+                            diff_summary=\Sprintf("(+%s, -%s)", adds, dels); \
                         } else { \
-                            color_code=\"\\033[0m\"; /* No Color */ \
+                            color_code="\\033[0m"; /* No Color */ \
                         } \
                         \
                         # Print formatted line \
-                        printf \"%s%s\\033[0m | %s\n\", color_code, filepath, diff_summary; \
+                        printf "%s%s\\033[0m | %s\n", color_code, filepath, diff_summary; \
                         } \
                     "; \
         }; f
