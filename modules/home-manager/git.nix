78f2bafff68f5a88d8c1ee15e3e38b064c2522c1
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-08 09:01:00 -0500

    Generation #260

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -31,48 +31,9 @@
             git log --pretty=format:"%Cred%H%Creset - %C(yellow)%an%Creset, %C(cyan)%ar%Creset%n%s" \
                     --numstat \
                     --diff-filter=ACDMR \
-                    --name-only \
-                    --follow \
                     --no-color \
                     "$@" \
-                    | awk " \
-                        BEGIN { \
-                            FS=\"\\\\t\"; \ # Escape backslash for awk
-                            printf \"\\\\n\"; \ # Escape backslash for awk
-                            last_commit=\"\"; \
-                        } \
-                        /^.{40} - / { \
-                            if (last_commit != \"\") { \
-                                printf \"\\\\n\"; \ # Escape backslash for awk
-                            } \
-                            print \\\$0; \ # Escape $ for shell
-                            last_commit = \\\$1; \ # Escape $ for shell
-                            next; \
-                        } \
-                        /^[0-9\\\\-]+/ { \ # Escape backslash for awk regex, then for shell (total of 4 backslashes)
-                            adds=\\\$1; \
-                            dels=\\\$2; \
-                            filepath=\\\$3; \
-                            \
-                            color_code=\"\"; \
-                            diff_summary=\"\"; \
-                            \
-                            if (adds == \"-\" && dels != \"-\") { \
-                                color_code=\"\\\\033[0;31m\"; \ # Escape backslashes for awk
-                                diff_summary=\"(Deleted)\"; \
-                            } else if (adds != \"-\" && dels == \"-\") { \
-                                color_code=\"\\\\033[0;32m\"; \ # Escape backslashes for awk
-                                diff_summary=\"(Added)\"; \
-                            } else if (adds != \"-\" && dels != \"-\") { \
-                                color_code=\"\\\\033[0;33m\"; \ # Escape backslashes for awk
-                                diff_summary=sprintf(\"(+%s, -%s)\", adds, dels); \ # sprintf for safety
-                            } else { \
-                                color_code=\"\\\\033[0m\"; \ # Escape backslashes for awk
-                            } \
-                            \
-                            printf \"%s%s\\\\033[0m | %s\\\\n\", color_code, filepath, diff_summary; \ # Escape backslashes for awk
-                        } \
-                    "; \
+                    | awk "BEGIN { FS=\"\\\\t\"; printf \"\\\\n\"; last_commit=\"\"; } /^.{40} - / { if (last_commit != \"\") { printf \"\\\\n\"; } print \\\$0; last_commit = \\\$1; next; } /^[0-9\\\\-]+/ { adds=\\\$1; dels=\\\$2; filepath=\\\$3; color_code=\"\"; diff_summary=\"\"; if (adds == \"-\" && dels != \"-\") { color_code=\"\\\\033[0;31m\"; diff_summary=\"(Deleted)\"; } else if (adds != \"-\" && dels == \"-\") { color_code=\"\\\\033[0;32m\"; diff_summary=\"(Added)\"; } else if (adds != \"-\" && dels != \"-\") { color_code=\"\\\\033[0;33m\"; diff_summary=sprintf(\"(+%s, -%s)\", adds, dels); } else { color_code=\"\\\\033[0m\"; } printf \"%s%s\\\\033[0m | %s\\\\n\", color_code, filepath, diff_summary; }"; \
         }; f
       '';
     };
