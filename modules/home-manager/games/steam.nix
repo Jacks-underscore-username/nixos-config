f549884cdc2a3f55f956110f9ade6bd09f64360c
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-08 17:50:57 -0500

    Generation #273

--- modules/home-manager/games/steam.nix
+++ modules/home-manager/games/steam.nix
@@ -2,44 +2,26 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  persist = home.persistence."/persist/home/jackc";
+in {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    # Crucial for many games, especially those needing specific runtime components
-    # Add common 32-bit libraries that Proton/Wine often rely on.
-    # These correspond to the Windows DLLs.
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = with pkgs; [
-      # Networking
       nss_latest
-      zlib # Often needed by networking components
-
-      # General system libs often implicitly linked or needed
-      alsa-lib # Audio
-      libpulseaudio # Audio (if you use PulseAudio)
-      # libx11 # Xorg core
-      # libxext # X extension library
-      # libxfixes # X fixes
-      # libxrandr # X RandR extension
-      # libxrender # X Render extension
-      # libxi # XInput
-      # libxcb # X protocol C-language Binding
-      # libxxf86vm # XFree86-VidMode Extension
-
-      # Graphics/Vulkan (even if you have hardware.opengl enabled)
-      vulkan-loader # Ensure Vulkan loader is present
-      # For NVIDIA users with Vulkan issues:
-      # libnvidia-container # Often useful for NVIDIA and containers/Proton
+      zlib
+      alsa-lib
+      libpulseaudio
+      vulkan-loader
     ];
   };
-
   hardware.opengl = {
     enable = true;
-    driSupport32Bit = true; # VERY IMPORTANT!
-    # If NVIDIA:
-    # enableNvidia = true;
-    # package = config.hardware.nvidia.drivers.package;
+    driSupport32Bit = true;
   };
+
+  persist = persist ++ [];
 }
