88a24fbed0963e6f088fb6cf8413d5a9bd02e535
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-13 14:12:17 -0500

    Generation #295

--- modules/home-manager/misc/spicetify.nix
+++ modules/home-manager/misc/spicetify.nix
@@ -4,22 +4,22 @@
   inputs,
   ...
 }: {
-  # programs.spicetify = {
-  #   enable = true;
-  #   colorScheme = "Forest";
-  #   alwaysEnableDevTools = true;
-  #   enabledExtensions = with inputs.spicetify-nix.legacyPackages.${pkgs.system}.extensions; [
-  #     adblockify
-  #     hidePodcasts
-  #     shuffle
-  #     trashbin
-  #     goToSong
-  #     playlistIntersection
-  #     showQueueDuration
-  #     history
-  #     betterGenres
-  #   ];
-  # };
+  programs.spicetify = {
+    enable = true;
+    colorScheme = "Forest";
+    alwaysEnableDevTools = true;
+    enabledExtensions = with inputs.spicetify-nix.legacyPackages.${pkgs.system}.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle
+      trashbin
+      goToSong
+      playlistIntersection
+      showQueueDuration
+      history
+      betterGenres
+    ];
+  };
 
   # home.packages = home.packages ++ [pkgs.spotify-tui];
 }
