65e2124aad7616d405b7b3122f6704e7e8e8e913
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-20 16:57:13 -0500

    Generation #331

--- modules/home-manager/system/hyprland.nix
+++ modules/home-manager/system/hyprland.nix
@@ -39,9 +39,9 @@
         #   rotate.length = 10;
         # };
         windowrule = [
-          "float, class:jetbrains-idea-ce"
+          # "float, class:jetbrains-idea-ce"
           # "fullscreen, class:jetbrains-idea-ce"
-          "size 100 100, class:jetbrains-idea-ce"
+          # "size 100 100, class:jetbrains-idea-ce"
         ];
         monitor = config.hyprland.monitors;
         general = {
