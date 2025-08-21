ad0b020c3821f0c0a2f87f319b99d8eac5eefafd
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-20 20:55:59 -0500

    Generation #342

--- modules/home-manager/system/hyprland.nix
+++ modules/home-manager/system/hyprland.nix
@@ -33,7 +33,7 @@
           "float, class:clipse"
           "size 622 652, class:clipse"
           "rounding 25, class:clipse"
-          "opacity 0.75, class:clipse"
+          "opacity 0.75, class:.*"
         ];
         # "plugin:dynamic-cursors" = {
         #   shake.enabled = false;
