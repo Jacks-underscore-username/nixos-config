9b12add245f114521c4da65eb0d95ab4670d4b71
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-21 12:24:16 -0500

    Generation #370

--- modules/home-manager/scripts/watchWindow.nix
+++ modules/home-manager/scripts/watchWindow.nix
@@ -1,4 +1,4 @@
 {pkgs}:
 pkgs.writeShellScriptBin "watchWindow" ''
-  hyprctl dispatch exec "kitty --class watchWindow bun --eval \"setInterval(()=>console.log(require('child_process').execSync('hyprctl activewindow').toString()), 100)\""
+  hyprctl dispatch exec "kitty --class watchWindow bun --eval \"setInterval(()=>{console.clear();console.log(require('child_process').execSync('hyprctl activewindow').toString())}, 100)\""
 ''
