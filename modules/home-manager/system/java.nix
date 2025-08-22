fb3dc7ab23700c38ad453f24f5406adb0f3fb3f4
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-22 13:32:31 -0500

    Generation #384

--- modules/home-manager/system/java.nix
+++ modules/home-manager/system/java.nix
@@ -1,9 +1,17 @@
 # inspo: https://discourse.nixos.org/t/fix-collision-with-multiple-jdks/10812/5
-{pkgs, ...}: {
+{pkgs, ...}: let
+  jdks = [
+    # jdk
+    # jdk17
+    # jdk11
+    pkgs.jetbrains.jdk
+    pkgs.jetbrains.jdk-no-jcef-17
+  ];
+in {
   programs.java = {
     enable = true;
     # This determines JAVA_HOME - set to Java 21 at the moment as that is what modern Minecraft uses
-    package = pkgs.jdk;
+    package = builtins.elemAt jdks 0;
   };
 
   home.sessionPath = ["$HOME/.jdks"];
@@ -12,11 +20,5 @@
         name = ".jdks/${jdk.version}";
         value = {source = jdk;};
       })
-      [
-        jdk
-        jdk17
-        jdk11
-        # pkgs.jetbrains.jdk
-        # pkgs.jetbrains.jdk-no-jcef-17
-      ]);
+      jdks);
 }
