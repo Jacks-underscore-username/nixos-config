2aa2e6401334ebb342338fa18df2533702e52d24
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-27 12:44:38 -0500

    Generation #419

--- modules/home-manager/scripts/rm-regex.nix
+++ modules/home-manager/scripts/rm-regex.nix
@@ -12,27 +12,27 @@ pkgs.writeShellScriptBin "rm-regex" ''
   declare -a files_to_delete=()
   while IFS= read -r -d $'\0' file; do
     files_to_delete+=("$file")
-  done < <(find . -print0 | grep -Pz "$1")
+  done < <(${pkgs.findutils}/bin/find . -print0 | ${pkgs.gnugrep}/bin/grep -Pz "$1")
 
-  if [ $\{#files_to_delete[@]} -eq 0 ]; then
+  if [ ''${#files_to_delete[@]} -eq 0 ]; then
     echo "No files found matching the regex '$1'."
     exit 0
   fi
 
   echo "The following files will be DELETED:"
-  for file in "$\{files_to_delete[@]}"; do
+  for file in "''${files_to_delete[@]}"; do
     echo "- '$file'"
   done
   echo "--------------------------------------------------"
 
-  read -p "Are you sure you want to permanently delete these $\{#files_to_delete[@]} files? (y/N): " confirmation
+  read -p "Are you sure you want to permanently delete these ''${#files_to_delete[@]} files? (y/N): " confirmation
   if [[ ! "$confirmation" =~ ^[yY]$ ]]; then
     echo "Operation cancelled. No files were deleted."
     exit 0
   fi
 
   echo "Proceeding with deletion..."
-  printf "%s\0" "$\{files_to_delete[@]}" | xargs -0 rm -rf
+  printf "%s\0" "''${files_to_delete[@]}" | xargs -0 rm -rf
 
   echo "Deletion complete."
 ''
