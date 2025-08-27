3280737a0f1e6d1eda711bfc8c3d1fab10f819f9
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-08-26 19:27:30 -0500

    Generation #414

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -175,6 +175,25 @@ in {
     localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = runtimeLibs;
   };
+  programs.steam.package = pkgs.steam.override {
+    extraPkgs = pkgs':
+      with pkgs';
+        [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib # Provides libstdc++.so.6
+          libkrb5
+          keyutils
+          # Add other libraries as needed
+        ]
+        ++ runtimeLibs;
+  };
+
   environment.sessionVariables = {
     LD_LIBRARY_PATH = [
       "${pkgs.krb5}/lib"
