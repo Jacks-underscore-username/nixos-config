7447ea00be958c96f8e4c17235809bc863b6e37a
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-11-07 13:39:24 -0600

    Generation #503

--- modules/home-manager/scripts/watchBuffer.nix
+++ modules/home-manager/scripts/watchBuffer.nix
@@ -1,37 +1,66 @@
 {pkgs}:
 pkgs.writeShellScriptBin "watchBuffer" ''
-  format_bytes() {
-    local num=$1
-    if (( $(${pkgs.bc}/bin/bc -l <<< "$num > 1024 * 1024 * 1024") )); then
-      ${pkgs.coreutils}/bin/printf "%.2fG" "$(${pkgs.bc}/bin/bc -l <<< "scale=2; $num / (1024*1024*1024)")"
-    elif (( $(${pkgs.bc}/bin/bc -l <<< "$num > 1024 * 1024") )); then
-      ${pkgs.coreutils}/bin/printf "%.2fM" "$(${pkgs.bc}/bin/bc -l <<< "scale=2; $num / (1024*1024)")"
-    elif (( $(${pkgs.bc}/bin/bc -l <<< "$num > 1024") )); then
-      ${pkgs.coreutils}/bin/printf "%.2fK" "$(${pkgs.bc}/bin/bc -l <<< "scale=2; $num / 1024")"
-    else
-      ${pkgs.coreutils}/bin/printf "%sB" "$num"
-    fi
-  }
-  export -f format_bytes
-
-  # The main watch command
   ${pkgs.watch}/bin/watch -n1 '
     echo "--- Memory Buffers ---"
-    ${pkgs.gnugrep}/bin/grep -E "(Dirty|Writeback)" /proc/meminfo | \
-      ${pkgs.gawk}/bin/awk '"'"'{print $1 " " ENVIRON["format_bytes"]($2 * 1024)}'"'"'
+    ${pkgs.gnugrep}/bin/grep -E "(Dirty|Writeback|WritebackTmp)" /proc/meminfo | \
+      ${pkgs.gawk}/bin/awk '"'"'{
+        printf "%s", $1; # Print the key (e.g., Dirty:)
+        num_bytes = $2 * 1024;
+        if (num_bytes > 1024 * 1024 * 1024) {
+          printf " %.2fG\n", num_bytes / (1024*1024*1024);
+        } else if (num_bytes > 1024 * 1024) {
+          printf " %.2fM\n", num_bytes / (1024*1024);
+        } else if (num_bytes > 1024) {
+          printf " %.2fK\n", num_bytes / 1024;
+        } else {
+          printf " %sB\n", num_bytes;
+        }
+      }'"'"'
 
-    echo -e "\n--- Disk I/O (sectors) ---"
-    for device in $(${pkgs.coreutils}/bin/ls /sys/block/); do
+    echo -e "\n--- Disk I/O (Cumulative) ---"
+    echo "Note: These values are cumulative since boot, not per-second rates."
+
+    for device in $(${pkgs.coreutils}/bin/ls -1 /sys/block/); do
+      if [[ "$device" == "loop"* ]]; then
+        continue
+      fi
+
       if [ -r "/sys/block/$device/stat" ]; then
-        stats=$(${pkgs.gawk}/bin/awk '"'"'{print $3, $7, $11}'"'"' "/sys/block/$device/stat")
+        stats=$(${pkgs.gawk}/bin/awk '"'"'{
+          rd_sectors=$3;
+          wr_sectors=$7;
+          io_ticks=$10; # This is total I/O time in milliseconds
 
-        read_sectors=$(echo "$stats" | ${pkgs.gawk}/bin/awk '"'"'{print $1}'"'"')
-        write_sectors=$(echo "$stats" | ${pkgs.gawk}/bin/awk '"'"'{print $2}'"'"')
-        total_io_time=$(echo "$stats" | ${pkgs.gawk}/bin/awk '"'"'{print $3}'"'"')
+          rd_bytes = rd_sectors * 512;
+          wr_bytes = wr_sectors * 512;
 
-        if [ "$read_sectors" -gt 0 ] || [ "$write_sectors" -gt 0 ]; then
-          ${pkgs.coreutils}/bin/printf "%s: Read: %s sectors, Write: %s sectors, I/O Time: %sms\n" \
-            "$device" "$read_sectors" "$write_sectors" "$total_io_time"
+          printf "%s: ", "'"$device"'";
+
+          if (rd_bytes > 1024 * 1024 * 1024) {
+            printf "Read: %.2fG ", rd_bytes / (1024*1024*1024);
+          } else if (rd_bytes > 1024 * 1024) {
+            printf "Read: %.2fM ", rd_bytes / (1024*1024);
+          } else if (rd_bytes > 1024) {
+            printf "Read: %.2fK ", rd_bytes / 1024;
+          } else {
+            printf "Read: %sB ", rd_bytes;
+          }
+
+          if (wr_bytes > 1024 * 1024 * 1024) {
+            printf "Write: %.2fG ", wr_bytes / (1024*1024*1024);
+          } else if (wr_bytes > 1024 * 1024) {
+            printf "Write: %.2fM ", wr_bytes / (1024*1024);
+          } else if (wr_bytes > 1024) {
+            printf "Write: %.2fK ", wr_bytes / 1024;
+          } else {
+            printf "Write: %sB ", wr_bytes;
+          }
+
+          printf "I/O Time: %.2fs\n", io_ticks / 1000;
+        }'"'"' "/sys/block/$device/stat")
+
+        if [ -n "$stats" ]; then
+          echo "$stats"
         fi
       fi
     done
