b3a5b01efcdf0bbf791483795fe190f529aeb31e
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-11-07 12:54:48 -0600

    Generation #495

--- modules/home-manager/scripts/denode.nix
+++ modules/home-manager/scripts/denode.nix
@@ -11,8 +11,6 @@ pkgs.writeShellScriptBin "denode" ''
       parent_dir=$(dirname "$node_modules_folder")
 
       if [[ -f "$parent_dir/package-lock.json" || -f "$parent_dir/bun.lock" ]]; then
-        echo "Found: $node_modules_folder (with associated lock file in $parent_dir)"
-
         folder_size_bytes=$(du -sb "$node_modules_folder" 2>/dev/null | awk '{print $1}')
 
         if [ -z "$folder_size_bytes" ]; then
@@ -22,12 +20,9 @@ pkgs.writeShellScriptBin "denode" ''
         echo "Size: $(numfmt --to=iec-i --suffix=B --padding=7 "$folder_size_bytes")"
 
         echo "Deleting '$node_modules_folder'..."
-        # rm -rf "$node_modules_folder"
-        echo "'$node_modules_folder' deleted."
+         rm -rf "$node_modules_folder"
 
         total_space_saved_bytes=$((total_space_saved_bytes + folder_size_bytes))
-      else
-        echo "Skipping: $node_modules_folder (no 'package-lock.json' or 'bun.lock' found in $parent_dir)"
       fi
   done < <(find "$target_directory" -type d -name "node_modules" -not -path "*/node_modules/*")
 
