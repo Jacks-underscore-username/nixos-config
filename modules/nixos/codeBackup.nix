f9abb8d413cdaa3cb9a89f7dd20cd170dcf9bac9
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-11-10 17:48:34 -0600

    Generation #516

--- modules/nixos/codeBackup.nix
+++ modules/nixos/codeBackup.nix
@@ -1,16 +1,23 @@
-{...}: {
+{pkgs, ...}: let
+  user = "jackc";
+in {
   systemd.services.codeBackup = {
     enable = true;
     description = "Backup the code folder";
     after = ["network-online.target"];
-    wantedBy = ["multi-user.target"];
+    wants = ["network-online.target"];
+    wantedBy = ["multi-user.target" "shutdown.target"];
     unitConfig = {
       Type = "oneshot";
-      Conflicts = ["shutdown.target"];
-      Before = ["shutdown.target"];
+      ConditionUser = user;
+      # Conflicts = ["shutdown.target"];
+      # Before = ["shutdown.target"];
+
+      User = user;
+      Environment = ["HOME=/home/${user}"];
     };
     script = ''
-      echo "Hello World!"
+      ${pkgs.openssh}/bin/ssh 192.168.4.62 'echo "Hello from the other server!" | wall'
     '';
   };
 }
