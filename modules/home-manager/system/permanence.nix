95dfec0e98d5ce13198417bdd09af7881badc082
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-11-13 18:21:33 -0600

    Generation #522

--- modules/home-manager/system/permanence.nix
+++ modules/home-manager/system/permanence.nix
@@ -26,7 +26,7 @@
       ".compose-cache"
 
       ".factorio"
-      # ".local/share/Celeste"
+      ".local/share/Celeste"
       # Something for celeste I think
       # ".compose-cache/l4_030_04b84530_0e7768ba"
       "Desktop"
