66d876f8fcb183006fea51bfc3dc3f1bcc9b3a2a
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-11-29 23:36:16 -0600

    Generation #542

--- modules/home-manager/system/hyprland.nix
+++ modules/home-manager/system/hyprland.nix
@@ -53,6 +53,7 @@
             Celeste = 1;
             "shapez 2" = 1;
             "Among Us" = 1;
+            "R.E.P.O." = 1;
           };
         # "plugin:dynamic-cursors" = {
         #   shake.enabled = false;
