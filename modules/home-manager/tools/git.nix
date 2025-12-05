0f48de9a98c1557aa5d12a68aa685ae82fdf1bdd
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-12-05 09:18:58 -0600

    Generation #545

--- modules/home-manager/tools/git.nix
+++ modules/home-manager/tools/git.nix
@@ -20,15 +20,15 @@
         };
       };
       safe.directory = ["/persist/nixos"];
-    };
-    aliases = {
-      dlc = "reset --soft HEAD^1";
-      dlcs = "reset --soft HEAD^1";
-      dlch = "reset --hard HEAD^1";
-      slc = ''!f() { LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B); git reset --soft HEAD^1 && git add -A && git commit -m "$LAST_COMMIT_MESSAGE" && git push --force; }; f'';
-      acp = ''!f() { git add -A && git commit -m "$@" && git push; }; f'';
-      acpf = ''!f() { git add -A && git commit -m "$@" && git push --force; }; f'';
-      plog = "!git_plog";
+      aliases = {
+        dlc = "reset --soft HEAD^1";
+        dlcs = "reset --soft HEAD^1";
+        dlch = "reset --hard HEAD^1";
+        slc = ''!f() { LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B); git reset --soft HEAD^1 && git add -A && git commit -m "$LAST_COMMIT_MESSAGE" && git push --force; }; f'';
+        acp = ''!f() { git add -A && git commit -m "$@" && git push; }; f'';
+        acpf = ''!f() { git add -A && git commit -m "$@" && git push --force; }; f'';
+        plog = "!git_plog";
+      };
     };
   };
 }
