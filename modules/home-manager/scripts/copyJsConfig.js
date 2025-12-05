34773dc4378cf730a33735869d2bbdb24136bc12
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-12-05 09:24:43 -0600

    Generation #545

--- modules/home-manager/scripts/copyJsConfig.js
+++ modules/home-manager/scripts/copyJsConfig.js
@@ -1,5 +1,12 @@
 const fs = require('node:fs')
 const path = require('node:path')
-for (const file of ['biome.jsonc', 'jsconfig.json'])
-  if (!fs.existsSync(path.join(process.argv[2], file)))
-    fs.symlinkSync(path.join('/persist/nixos', file), path.join(process.argv[2], file))
+const fileMap = {
+  'biome.jsonc': undefined,
+  'jsconfig.json': undefined,
+  '.gitignore': 'jsConfig.gitignore'
+}
+for (let [name, filePath] of Object.entries(fileMap)) {
+  filePath = filePath ?? name
+  if (!fs.existsSync(path.join(process.argv[2] ?? '', name)))
+    fs.symlinkSync(path.join('/persist/nixos', name), path.join(process.argv[2] ?? '', filePath))
+}
