17665a135bdec74bd8f3954cf004d521370b0e54
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2025-12-05 09:31:29 -0600

    Generation #545

--- modules/home-manager/scripts/copyJsConfig.js
+++ modules/home-manager/scripts/copyJsConfig.js
@@ -1,12 +1,22 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const fileMap = {
+/** @type {{ [key: string]: string | undefined }} */
+const linkMap = {
   'biome.jsonc': undefined,
-  'jsconfig.json': undefined,
+  'jsconfig.json': undefined
+}
+for (let [name, filePath] of Object.entries(linkMap)) {
+  filePath = filePath ?? name
+  if (!fs.existsSync(path.join(process.argv[2] ?? '', name)))
+    fs.symlinkSync(path.join('/persist/nixos', filePath), path.join(process.argv[2] ?? '', name))
+}
+
+/** @type {{ [key: string]: string | undefined }} */
+const fileMap = {
   '.gitignore': 'jsConfig.gitignore'
 }
 for (let [name, filePath] of Object.entries(fileMap)) {
   filePath = filePath ?? name
   if (!fs.existsSync(path.join(process.argv[2] ?? '', name)))
-    fs.symlinkSync(path.join('/persist/nixos', name), path.join(process.argv[2] ?? '', filePath))
+    fs.copyFileSync(path.join('/persist/nixos', filePath), path.join(process.argv[2] ?? '', name))
 }
