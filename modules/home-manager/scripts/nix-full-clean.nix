acfec2df3093c0e2d00c97c31ec2f2695cb1c0f4
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-01-28 20:26:53 -0600

    Generation #578

--- modules/home-manager/scripts/nix-full-clean.nix
+++ modules/home-manager/scripts/nix-full-clean.nix
@@ -2,7 +2,7 @@
 pkgs.writeShellScriptBin "nix-full-clean" ''
   sudo nix-collect-garbage -d &&
   nix store gc &&
-  sudo nix store optimize &&
+  sudo nix store optimise &&
   sudo nix profile wipe-history &&
   nix-store --verify --check-contents --repair &&
   reboot
