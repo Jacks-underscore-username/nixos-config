0d4f1b7c49501146870a1598c0aecef7a33a570d
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-02-22 12:46:36 -0600

    Generation #596

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -9,6 +9,7 @@
   programs.starship = {
     enable = true;
     settings = {
+      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
