2ded932ca1802f725536b4bbf2064001953b397a
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-02-22 12:56:26 -0600

    Generation #597

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -5,13 +5,38 @@
 
   programs.fish = {
     enable = true;
-    shellInit = "starship init fish | source";
+    shellInit = ''
+      # Fish syntax highlighting
+      set -g fish_color_autosuggestion '555'  'brblack'
+      set -g fish_color_cancel -r
+      set -g fish_color_command --bold
+      set -g fish_color_comment red
+      set -g fish_color_cwd green
+      set -g fish_color_cwd_root red
+      set -g fish_color_end brmagenta
+      set -g fish_color_error brred
+      set -g fish_color_escape 'bryellow'  '--bold'
+      set -g fish_color_history_current --bold
+      set -g fish_color_host normal
+      set -g fish_color_match --background=brblue
+      set -g fish_color_normal normal
+      set -g fish_color_operator bryellow
+      set -g fish_color_param cyan
+      set -g fish_color_quote yellow
+      set -g fish_color_redirection brblue
+      set -g fish_color_search_match 'bryellow'  '--background=brblack'
+      set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+      set -g fish_color_user brgreen
+      set -g fish_color_valid_path --underline
+
+      # Install Starship
+      starship init fish | source
+    '';
   };
 
   programs.starship = {
     enable = true;
     settings = {
-      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
